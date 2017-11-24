a17ffabb4c11de96e09677eebb2ae8f71c1535c5
mayomayo25 <10bvb25hal45016@gmail.com>
2017-11-24 12:56:40 +0900

    [update] add url-loader

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,10 @@ module.exports = {
   			test: /\.styl$/,
         loader: ['style-loader','css-loader','stylus-loader'],
       },
+      {
+        test: /\.(jpg|png|gif)$/,
+        loader: 'url-loader',
+      },
     ],
   },
 
