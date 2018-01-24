516ba51b5ec1626ed165181efbeb79b7a7b59a91
miwa_shuntaro <>
2018-01-25 01:53:42 +0900

    sp横のfirstview修正＆とりあえず完成

--- src/js/app.js
+++ src/js/app.js
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // 他のjs読み込み
-import {Main} from './main'
 import {Query} from './query'
 
 // stylus読み込み
 import styles from '../stylus/style.styl'
-
-Main();
 Query();
